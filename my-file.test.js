ca694530ed98b81082fcbeeb5df033f483cacc37
gabrielmissio <gabriel.missio@compasso.com.br>
2023-11-08 21:29:33 -0300

    Add jest and the prompt test

---
+++ my-file.test.js
@@ -0,0 +1,11 @@
+require('./customMatchers')
+const myInput = require('./demo-input.json')
+// Ensure you have create a file named demo-input.json in the same directory as this file
+
+it('Lorem Ipsum', async () => {
+    const response = {
+        body: myInput
+    }
+
+    // paste the GTP output here
+})
